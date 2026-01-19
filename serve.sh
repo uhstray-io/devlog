ee216c03abcb73e86ef5b7e22a365e15a04d8c7b
Jacob <jacobedwardhaig@gmail.com>
2026-01-19 18:22:26 -0500

    updated serve

--- serve.sh
+++ serve.sh
@@ -2,4 +2,7 @@
 
 cd public
 
-static-web-server --port 8000 -d public/
+
+# https://static-web-server.net/configuration/command-line-arguments/
+
+static-web-server --port 8000 -d ./public --log-level info --log-with-ansi true --log-remote-address true
