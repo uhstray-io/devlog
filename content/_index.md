8263d41e56ad35d674d8dbdc3198ddcf73d75b65
Jacob <jacobedwardhaig@gmail.com>
2026-01-19 19:32:35 -0500

    Updated Serve

--- content/_index.md
+++ content/_index.md
@@ -10,3 +10,4 @@ tags = ["index"]
 
 This is the place where we share updates, thoughts, and ideas about the development of our projects. Here, you'll find posts that cover a wide range of topics, from technical insights to personal reflections.
 
+This is where we share our journey through development. Get started by exploring our latest posts and feel free to reach out if you have any questions or suggestions! 
--- serve.sh
+++ serve.sh
@@ -2,4 +2,21 @@
 
 # https://static-web-server.net/configuration/command-line-arguments/
 
-static-web-server --port 8000 -d ./public --log-level info --log-with-ansi true --log-remote-address true
+# Detect IP address for Linux and Windows
+
+# Linux
+if [[ "$(uname -s)" == "Linux" ]]; then
+	IP_ADDRESS=$(hostname -I | awk '{print $1}')
+
+# Windows (Git Bash, Cygwin, MSYS)
+elif [[ "$(uname -s)" == "MINGW"* || "$(uname -s)" == "CYGWIN"* || "$(uname -s)" == "MSYS"* ]]; then
+	IP_ADDRESS=$(ipconfig | grep -E "IPv4" | grep -v "127.0.0.1" | awk -F: '{print $2}' | awk '{print $1}' | grep '^192.168' | head -n 1)
+    # Fallback to localhost if no suitable IP found
+	if [ -z "$IP_ADDRESS" ]; then
+		IP_ADDRESS="127.0.0.1"
+	fi
+else
+	IP_ADDRESS="127.0.0.1"
+fi
+
+./static-web-server --host "$IP_ADDRESS" --port 8000 -d ./public --log-level info --log-with-ansi true --log-remote-address true
